4e26d814f8b93df94fbd55aee1faf10fa0c05ff4
namyulbae <bny64@naver.com>
2019-03-13 17:59:56 +0900

    grid

---
+++ pqgrid/grid1.js
@@ -0,0 +1,24 @@
+
+var grid1 = null;
+
+makeGrid1();
+grid1.pqGrid('option', 'dataModel.data', gridData1);
+grid1.pqGrid('refreshDataAndView');
+
+function makeGrid1(){
+
+    var colModel = [
+        {title : '번호', width:'300', dataIndx:'number'},
+        {title : '메뉴명', width:'300', dataIndx:'menu'},
+        {title : '조회수', width:'300', dataIndx:'count'},
+        {title : '조회수%', width:'300', dataIndx:'countPer'},
+    ];
+
+    grid1 = $("#grid1").pqGrid({
+        width:'1000',
+        height:'300',
+        colModel : colModel,
+        scrollModel : {autoFit:true},
+    });
+
+}
---
+++ pqgrid/gridData1.js
@@ -0,0 +1,78 @@
+var gridData1 = [
+    {
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    },{
+        number : 1,
+        menu : 'A',
+        count:10000,
+        countPer:10.5
+    }
+]
