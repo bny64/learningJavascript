d99a7f4b92795b7f8908b0c64829f278d9a96b3f
namyul <bny64@naver.com>
2023-10-16 07:00:17 +0900

    생활 코딩 4. 이벤트

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,57 @@
 import "./App.css";
 import React, { Component } from "react";
-import TOC from './components/TOC';
-import Content from './components/Content';
-import Subejct from './components/Subject';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subejct from "./components/Subject";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "read",
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for iteractive" },
+      ],
+    };
+  }
   render() {
+    let _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subejct title="WEB" sub="world wide web!"></Subejct>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        {/* <Subejct
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subejct> */}
+        <header>
+          <h1>
+            <a
+              href="/"
+              onClick={function (e) {
+                e.preventDefault();
+                // this.state.mode = 'welcome';
+                this.setState({
+                  mode: "welcome",
+                });
+              }.bind(this)}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
