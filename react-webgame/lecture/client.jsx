86a244490d60124a277783df263eb10fefea2feb
namyul <bny64@naver.com>
2023-10-24 22:10:40 +0900

    zerocho 섹션 1 숫자야구 - prop, react의 데이터 감지 개념

--- react-webgame/lecture/client.jsx
+++ react-webgame/lecture/client.jsx
@@ -2,5 +2,6 @@ const React = require("react");
 const ReactDOM = require("react-dom/client");
 
 import WordRelay from "./WordRelay-hooks";
+import NumberBaseball from './NumberBaseball';
 
-ReactDOM.createRoot(document.querySelector("#root")).render(<WordRelay />);
+ReactDOM.createRoot(document.querySelector("#root")).render(<NumberBaseball />);
