706788cb5972e77de9fa32645d9dba249a916d7b
bny64 <bny64@naver.com>
2023-10-25 17:57:33 +0900

    개인연습

--- react-webgame/0.개인연습/react-test-app/src/App.js
+++ react-webgame/0.개인연습/react-test-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MainBoard from "./MainBoard";
+import Board from "./Board";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <MainBoard />
+      <Board />
     </div>
   );
 }
--- react-webgame/0.개인연습/react-test-app/src/MainBoard.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const MainBoard = () => {
-  return (
-    <div>HELLO</div>
-  )
-};
-
-export default MainBoard;
